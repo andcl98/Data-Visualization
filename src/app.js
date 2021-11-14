ba51bd05fa686e3c72d7dcb7e5ae7d26a1ca7b6f
dolmeng-E <bny9889@gmail.com>
2021-11-14 19:15:40 +0900

    add face

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 import React from "react";
 import asea from "./asea.png";
 import "./App.css";
+
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={asea} className="App-logo" alt="asea-building" />
         <h1>YAOOOOOOA!</h1>
+        <svg width={width} height={height}>
+          <circle
+            cx={centerX}
+            cy={centerY}
+            r={centerY - strokeWidth / 2}
+            fill="yellow"
+            stroke="black"
+            strokeWidth={strokeWidth}
+          />
+          <circle
+            cx={centerX - eyeOffsetX}
+            cy={centerY - eyeOffsetY}
+            r={eyeRadius}
+          />
+          <circle
+            cx={centerX + eyeOffsetX}
+            cy={centerY - eyeOffsetY}
+            r={eyeRadius}
+          />
+        </svg>
       </header>
     </div>
   );
